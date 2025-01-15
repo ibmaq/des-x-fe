805746cacac63303c4f7b349b633b1ce7bb0a44c
ibmaq <ibrahimaq99@gmail.com>
2025-01-15 18:02:33 +0500

    fonts + some custom tw configs added

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,16 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        bebas: "var(--font-bebas-neue)",
+      },
+      fontSize: {
+        48: "192px",
+        49: "196px",
+      },
+      letterSpacing: {
+        5.04: "5.04px",
+      },
     },
   },
   plugins: [],
