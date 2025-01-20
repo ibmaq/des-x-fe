60fc031ef416ac7ee862afde32433b624f7b747f
grit-over-fear <gritoverfear@gmail.com>
2025-01-20 20:37:41 +0500

    feat(responsive views): responsive views completed

--- app/work/dably/(components)/HeaderSection.js
+++ app/work/dably/(components)/HeaderSection.js
@@ -3,16 +3,16 @@ import { Button } from "@/app/reusable-components/Button";
 export default function HeaderSection({ children, heading }) {
   return (
     <div className="w-full max-w-182 mx-auto lg:py-28 py-10 md:py-20 flex flex-col justify-center items-center md:gap-8 gap-5 max-md:px-4">
-      <div className="h-8 md:hidden"/>
+      <div className="h-8 md:hidden" />
       <div className="w-fit flex flex-col md:gap-5 gap-2">
         <div className="flex flex-col gap-3 justify-center items-center">
           {children}
           {/* <DablyIcon /> */}
-          <h3 className="md:text-49/none text-30/none -tracking-5.04 text-cust-white font-bebas">
+          <h3 className="md:text-49/none text-28/none -tracking-5.04 text-cust-white font-bebas">
             {heading}
           </h3>
         </div>
-        <p className="font-medium text-xl/6 -tracking-0.19 text-gray-200 text-center ">
+        <p className="font-medium text-xl/6 -tracking-0.19 text-gray-200 text-center">
           Where creativity meets strategy. We don’t just design—we craft
           experiences that captivate your audience, connect with their needs,
           and drive results that matter. Bold, innovative, and tailored to your
