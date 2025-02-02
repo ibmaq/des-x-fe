0bfa0179f6286562b913e36f6fc58bce67ed37c6
ibmaq <ibrahimaq99@gmail.com>
2025-02-03 02:32:09 +0500

    lp:latestWorks section updated for all views

--- app/(landing)/(components)/LatestWorks.js
+++ app/(landing)/(components)/LatestWorks.js
@@ -9,6 +9,9 @@ export const LatestWorks = () => {
     {
       id: 0,
       title: "Pendfunds",
+      desc: "Lorem ipsum dolor sit amet consectetur. Faucibus sed enim donec in nec integer. Quisque ut etiam fringilla sed viverra viverra. Tempus in velit eu cras pellentesque. Viverra mauris gravida donec eu metus rhoncus adipiscing amet et.",
+      pillContent: "We help raise 100k+ USD for Pendfunds 🔥 - See Case Study",
+      update: true,
       img: "/images/works/pendfunds.png",
       services: ["UX/UI Design", "Web Development"],
       link: "/work/pendfunds",
@@ -16,6 +19,9 @@ export const LatestWorks = () => {
     {
       id: 1,
       title: "Dably",
+      desc: "Lorem ipsum dolor sit amet consectetur. Faucibus sed enim donec in nec integer. Quisque ut etiam fringilla sed viverra viverra. Tempus in velit eu cras pellentesque. Viverra mauris gravida donec eu metus rhoncus adipiscing amet et.",
+      pillContent: "Web3 based startup",
+      update: false,
       img: "/images/works/dably.png",
       services: ["UX/UI Design", "Web Development"],
       link: "/work/dably",
@@ -23,6 +29,9 @@ export const LatestWorks = () => {
     {
       id: 2,
       title: "Snovos",
+      desc: "Lorem ipsum dolor sit amet consectetur. Faucibus sed enim donec in nec integer. Quisque ut etiam fringilla sed viverra viverra. Tempus in velit eu cras pellentesque. Viverra mauris gravida donec eu metus rhoncus adipiscing amet et.",
+      update: false,
+      pillContent: "Full UI Revamp 🚀",
       img: "/images/works/snovos.png",
       services: ["UX/UI Re-Design"],
       link: "coming-soon",
@@ -30,6 +39,9 @@ export const LatestWorks = () => {
     {
       id: 3,
       title: "Rentto",
+      desc: "Lorem ipsum dolor sit amet consectetur. Faucibus sed enim donec in nec integer. Quisque ut etiam fringilla sed viverra viverra. Tempus in velit eu cras pellentesque. Viverra mauris gravida donec eu metus rhoncus adipiscing amet et.",
+      update: false,
+      pillContent: "Full UI Revamp 🚀",
       img: "/images/works/rentto.png",
       services: ["UX/UI Re-Design"],
       link: "coming-soon",
@@ -37,59 +49,120 @@ export const LatestWorks = () => {
   ];
 
   return (
-    <div className="flex flex-col gap-15 items-center justify-center py-30 px-4 lg:px-20">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-9.5">
+    <div className="flex flex-col gap-10 items-center justify-center px-4 c-md:px-8 2xl:px-15 3xl:px-33.75 py-35 contained">
+      <div className="flex flex-col gap-3 items-center text-center">
+        <h2 className="section-title">Who we have Helped</h2>
+        <p className="text-gray-300 text-base c-md:text-xl/normal font-medium capitalize">
+          Today's users judge fast and scroll faster. We create{" "}
+          <br className="max-sm:hidden" /> designs that stop them in their
+          tracks through...
+        </p>
+      </div>
+      <div className="flex flex-col gap-10">
         {worksData.map((work, workIndex) => (
           <Link
             key={workIndex}
             href={work.link !== "coming-soon" ? work.link : "#"}
-            className={"relative group"}
+            className="p-5 border border-c-white/30 rounded-xl bg-c-black-2"
             scroll={work.link !== "coming-soon" ? true : false}
           >
             <motion.div
-              className="flex flex-col gap-5 hover:-translate-y-4 transition-transform duration-300 ease-in-out"
+              className="flex flex-col lg:flex-row gap-10"
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               viewport={{ once: true, amount: 0.2 }}
             >
-              <div className="border border-c-white/30 rounded-md overflow-hidden">
-                <Image
-                  src={work.img}
-                  alt={work.title}
-                  width={1000}
-                  height={90}
-                />
-              </div>
-              <div className="flex max-sm:flex-col justify-between lg:items-center uppercase">
-                <p className="font-extrabold text-xl lg:text-2xl/1.2 text-white">
-                  {work.title}
-                </p>
-                <div className="flex gap-2 text-base lg:text-4.8/none text-gray-400 -tracking-0.19">
-                  {work.services.map((service, serviceIndex) => (
-                    <p key={serviceIndex}>{service}</p>
-                  ))}
+              <Image
+                src={work.img}
+                alt={work.title}
+                width={616}
+                height={333}
+                className="rounded-xl w-full lg:max-w-113.5 2xl:max-w-147 2xl:h-107.5"
+              />
+              <div className="flex flex-col gap-10 2xl:justify-between">
+                <div className="flex flex-col">
+                  <p className="text-13.5/none c-md:text-20/none text-c-white font-bebas -tracking-2">
+                    {work.title}
+                  </p>
+                  <p className="flex gap-2 text-sm/1.4 c-md:text-base/1.4 text-gray-400 capitalize">
+                    {work.desc}
+                  </p>
                 </div>
-              </div>
-              <div
-                className={`absolute inset-0 opacity-0 ${
-                  work.link === "coming-soon"
-                    ? "group-hover:opacity-100 cursor-default"
-                    : ""
-                } bg-black/90 flex justify-center items-center transition-opacity duration-300 ease-in-out`}
-              >
-                <div
-                  className={
-                    "uppercase rounded-2.5 backdrop-blur-10 p-5 border text-c-black bg-c-white border-white font-extrabold text-base lg:text-xl leading-none -tracking-0.19"
-                  }
-                >
-                  Coming Soon
+                <div className="flex flex-col gap-3">
+                  <div
+                    className={`pill-anim-border w-fit text-white bg-c-gray-1 ${
+                      work.update ? "px-1 c-md:pl-1 c-md:pr-2.5" : "px-2.5"
+                    } py-1`}
+                  >
+                    {work.update ? (
+                      <div className="pulse-shadow-wrapper"></div>
+                    ) : null}
+                    <div className="relative z-1 font-medium flex gap-3 items-center">
+                      {work.update ? (
+                        <div className="pl-2 pr-2.5 py-1 rounded-full border border-gray-600 flex gap-1.5 items-center bg-gray-700 max-c-md:w-20">
+                          <span className="size-2 shrink-0 animate-pulse text-white">
+                            <svg
+                              viewBox="0 0 9 8"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <circle
+                                cx="4.59541"
+                                cy="4"
+                                r="3"
+                                fill="currentColor"
+                              />
+                            </svg>
+                          </span>
+                          Update
+                        </div>
+                      ) : (
+                        <span className="size-1.5">
+                          <svg
+                            viewBox="0 0 6 6"
+                            fill="none"
+                            xmlns="http://www.w3.org/2000/svg"
+                          >
+                            <circle cx={3} cy={3} r={3} fill="white" />
+                          </svg>
+                        </span>
+                      )}
+                      <p
+                        className={`text-sm ${
+                          work.update ? "leading-5" : "leading-none"
+                        }`}
+                      >
+                        {work.pillContent}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="flex gap-2 text-lg/none lg:text-4.8/none text-gray-400 -tracking-0.19 uppercase">
+                    {work.services.map((service, serviceIndex) => (
+                      <div
+                        key={serviceIndex}
+                        className="flex items-center gap-1.5 whitespace-nowrap"
+                      >
+                        {serviceIndex !== 0 && (
+                          <span className="size-1.5">
+                            <svg
+                              viewBox="0 0 6 6"
+                              fill="none"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <circle cx={3} cy={3} r={3} fill="white" />
+                            </svg>
+                          </span>
+                        )}
+                        <p>{service}</p>
+                      </div>
+                    ))}
+                  </div>
                 </div>
               </div>
             </motion.div>
           </Link>
         ))}
       </div>
-      <Button text={"see all works"} icon={"arrow"} theme={"secondary"} />
     </div>
   );
 };
