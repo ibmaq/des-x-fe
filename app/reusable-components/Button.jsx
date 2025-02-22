41dc611755bb1c4dc1ca914dfbf2e8ac52d27dc3
ibmaq <ibrahimaq99@gmail.com>
2025-02-22 21:30:37 +0500

    contact: email updated + loader

--- app/reusable-components/Button.jsx
+++ app/reusable-components/Button.jsx
@@ -99,7 +99,6 @@ const Icon = ({ name, theme }) => {
         strokeWidth={2}
         strokeLinecap="round"
         strokeLinejoin="round"
-        className="animate-spin"
       >
         <path d="M21 12a9 9 0 1 1-6.219-8.56" />
       </svg>
